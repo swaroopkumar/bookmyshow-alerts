f47ae79c099e80528732180e1e1d580d43d3456a
Swaroop Kumar <swaroopkumar.badam@gmail.com>
2017-07-07 16:08:19 +0530

    initial commit

---
+++ bms-alert.py
@@ -0,0 +1,25 @@
+import re, urllib, smtplib
+
+from email.mime.text import MIMEText
+
+print "Enter the bookmyshow movie url (with double quotes).."
+myurl = "https://in.bookmyshow.com/buytickets/ninnu-kori-hyderabad/movie-hyd-ET00053894-MT/20170707"
+print "Enter the bookmyshow theatre name (with double quotes).."
+mytheatre = "svc-cinemas-eeshwar-attapur"
+my_regex = r"href=[\"\'].*" + mytheatre + r".*[\"\']"
+
+if re.search(my_regex, urllib.urlopen(myurl).read(), re.I):
+	fromaddr = "swaroopkumar.badam@live.com"
+	toaddrs = ["swaroopkumar.badam@gmail.com"];
+	msg = ("From: %s\r\nTo: %s\r\n" % (fromaddr, ", ".join(toaddrs)))
+	msg = msg + "Subject:Tickets available !! for " + mytheatre + "\r\n\r\n"
+	msg = msg + "Tickets available.\n"
+	server = smtplib.SMTP('smtp.live.com', 587)
+	server.ehlo()
+	server.starttls()
+	server.login("swaroopkumar.badam@live.com", "******")
+	server.sendmail(fromaddr, toaddrs, msg)
+	server.quit()
+	print "Tickets available !! for " + mytheatre
+else:
+    print "No Tickets yet :-( for " + mytheatre
