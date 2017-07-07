19dc9cd5596b4b3b08d478ab6c2a5192151bc02c
Swaroop Kumar <swaroopkumar.badam@gmail.com>
2017-07-07 17:28:38 +0530

    adding empty args check

--- bms-alert.py
+++ bms-alert.py
@@ -4,6 +4,10 @@ import re, urllib, smtplib, sys
 
 from email.mime.text import MIMEText
 
+if len(sys.argv) < 3:
+    print "Movie link and at least one theater name is required!"
+    sys.exit()
+
 myurl = sys.argv[1]
 print "Movie link:" + myurl
 mytheatre = sys.argv[2]
@@ -12,17 +16,17 @@ print "Theatre name:" + mytheatre
 my_regex = r"href=[\"\'].*" + mytheatre + r".*[\"\']"
 
 if re.search(my_regex, urllib.urlopen(myurl).read(), re.I):
-	fromaddr = "from@live.com"
-	toaddrs = ["to@gmail.com"];
-	msg = ("From: %s\r\nTo: %s\r\n" % (fromaddr, ", ".join(toaddrs)))
-	msg = msg + "Subject:Tickets available !! for " + mytheatre + "\r\n\r\n"
-	msg = msg + "Tickets available.\n"
-	server = smtplib.SMTP('smtp.live.com', 587)
-	server.ehlo()
-	server.starttls()
-	server.login("from@live.com", "****")
-	server.sendmail(fromaddr, toaddrs, msg)
-	server.quit()
-	print "Tickets available !! for " + mytheatre
+    fromaddr = "from@live.com"
+    toaddrs = ["to@gmail.com"];
+    msg = ("From: %s\r\nTo: %s\r\n" % (fromaddr, ", ".join(toaddrs)))
+    msg = msg + "Subject:Tickets available !! for " + mytheatre + "\r\n\r\n"
+    msg = msg + "Tickets available.\n"
+    server = smtplib.SMTP('smtp.live.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login("from@live.com", "****")
+    server.sendmail(fromaddr, toaddrs, msg)
+    server.quit()
+    print "Tickets available !! for " + mytheatre
 else:
     print "No Tickets yet :-( for " + mytheatre
