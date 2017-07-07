6db5461720be61bf5c0a387b0586aae24651f980
Swaroop Kumar <swaroopkumar.badam@gmail.com>
2017-07-07 16:55:15 +0530

    adding command line arguments

--- bms-alert.py
+++ bms-alert.py
@@ -1,23 +1,26 @@
-import re, urllib, smtplib
+#!/usr/bin/python
+
+import re, urllib, smtplib, sys
 
 from email.mime.text import MIMEText
 
-print "Enter the bookmyshow movie url (with double quotes).."
-myurl = "https://in.bookmyshow.com/buytickets/ninnu-kori-hyderabad/movie-hyd-ET00053894-MT/20170707"
-print "Enter the bookmyshow theatre name (with double quotes).."
-mytheatre = "svc-cinemas-eeshwar-attapur"
+myurl = sys.argv[1]
+print "Movie link:" + myurl
+mytheatre = sys.argv[2]
+print "Theatre name:" + mytheatre
+
 my_regex = r"href=[\"\'].*" + mytheatre + r".*[\"\']"
 
 if re.search(my_regex, urllib.urlopen(myurl).read(), re.I):
 	fromaddr = "from@live.com"
-	toaddrs = ["to@email.com"];
+	toaddrs = ["to@gmail.com"];
 	msg = ("From: %s\r\nTo: %s\r\n" % (fromaddr, ", ".join(toaddrs)))
 	msg = msg + "Subject:Tickets available !! for " + mytheatre + "\r\n\r\n"
 	msg = msg + "Tickets available.\n"
 	server = smtplib.SMTP('smtp.live.com', 587)
 	server.ehlo()
 	server.starttls()
-	server.login("from@live.com", "******")
+	server.login("from@live.com", "****")
 	server.sendmail(fromaddr, toaddrs, msg)
 	server.quit()
 	print "Tickets available !! for " + mytheatre
