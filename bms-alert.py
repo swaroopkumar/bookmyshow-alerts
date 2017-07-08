1be6b7f51f3c0fc86622cfae7a854e11f9205167
Swaroop Kumar <swaroopkumar.badam@gmail.com>
2017-07-08 11:48:19 +0530

    refactoring

--- bms-alert.py
+++ bms-alert.py
@@ -11,8 +11,8 @@ if len(sys.argv) < 4:
 email_ids = sys.argv[1]
 email_ids = email_ids.split(",")
 
-myurl = sys.argv[2]
-print "Movie link:" + myurl
+movie_url = sys.argv[2]
+print "Movie link:" + movie_url
 
 mytheatres = []
 length = len(sys.argv)
@@ -25,27 +25,29 @@ if length >= 6:
 
 print "Theatre names:" + ", ".join(mytheatres)
 
-html_text = urllib.urlopen(myurl).read()
+def send_email(email_ids, myurl, mytheatre):
+    fromaddr = "from@live.com"
+    toaddrs = email_ids
+    msg = ("From: %s\r\nTo: %s\r\n" % (fromaddr, ", ".join(toaddrs)))
+    movie_name = re.search(".*buytickets\/([a-z0-9\-]+)\/", myurl, re.I)
+    if movie_name:
+        movie_name = movie_name.group(1)
+    msg = msg + "Subject:Tickets available !! for " + movie_name + " at " + mytheatre + "\r\n\r\n"
+    msg = msg + "Tickets available.\n"
+    server = smtplib.SMTP('smtp.live.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login("from@live.com", "*****")
+    server.sendmail(fromaddr, toaddrs, msg)
+    server.quit()
+    return
+
+html_text = urllib.urlopen(movie_url).read()
 
 for mytheatre in mytheatres:
-
     my_regex = r"href=[\"\'].*" + mytheatre + r".*[\"\']"
-
     if re.search(my_regex, html_text, re.I):
-        fromaddr = "from@live.com"
-        toaddrs = email_ids
-        msg = ("From: %s\r\nTo: %s\r\n" % (fromaddr, ", ".join(toaddrs)))
-        movie_name = re.search(".*buytickets\/([a-z0-9\-]+)\/", myurl, re.I)
-        if movie_name:
-            movie_name = movie_name.group(1)
-        msg = msg + "Subject:Tickets available !! for " + movie_name + " at " + mytheatre + "\r\n\r\n"
-        msg = msg + "Tickets available.\n"
-        server = smtplib.SMTP('smtp.live.com', 587)
-        server.ehlo()
-        server.starttls()
-        server.login("from@live.com", "****")
-        server.sendmail(fromaddr, toaddrs, msg)
-        server.quit()
+        send_email(email_ids, movie_url, mytheatre)
         print "Tickets available !! for " + mytheatre
     else:
         print "No Tickets yet :-( for " + mytheatre
