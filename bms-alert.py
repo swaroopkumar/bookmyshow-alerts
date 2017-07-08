b1447717414e149076e4e230f6c075432251eb86
Swaroop Kumar <swaroopkumar.badam@gmail.com>
2017-07-08 10:29:57 +0530

    adding email ids as cmd line arguments

--- bms-alert.py
+++ bms-alert.py
@@ -4,21 +4,24 @@ import re, urllib, smtplib, sys
 
 from email.mime.text import MIMEText
 
-if len(sys.argv) < 3:
-    print "Movie link and at least one theater name is required!"
+if len(sys.argv) < 4:
+    print "Email, Movie link and at least one theater name is required!"
     sys.exit()
 
-myurl = sys.argv[1]
+email_ids = sys.argv[1]
+email_ids = email_ids.split(",")
+
+myurl = sys.argv[2]
 print "Movie link:" + myurl
 
 mytheatres = []
 length = len(sys.argv)
-if length >= 3:
-    mytheatres.append(sys.argv[2])
 if length >= 4:
     mytheatres.append(sys.argv[3])
 if length >= 5:
     mytheatres.append(sys.argv[4])
+if length >= 6:
+    mytheatres.append(sys.argv[5])
 
 print "Theatre names:" + ", ".join(mytheatres)
 
@@ -30,7 +33,7 @@ for mytheatre in mytheatres:
 
     if re.search(my_regex, html_text, re.I):
         fromaddr = "from@live.com"
-        toaddrs = ["to@gmail.com"];
+        toaddrs = email_ids
         msg = ("From: %s\r\nTo: %s\r\n" % (fromaddr, ", ".join(toaddrs)))
         movie_name = re.search(".*buytickets\/([a-z0-9\-]+)\/", myurl, re.I)
         if movie_name:
