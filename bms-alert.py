5c0c2335f4797a44ffa9a948d8c8bbfb197fc2d1
Swaroop Kumar <swaroopkumar.badam@gmail.com>
2017-07-08 07:49:03 +0530

    accepting 3 theater names

--- bms-alert.py
+++ bms-alert.py
@@ -10,23 +10,34 @@ if len(sys.argv) < 3:
 
 myurl = sys.argv[1]
 print "Movie link:" + myurl
-mytheatre = sys.argv[2]
-print "Theatre name:" + mytheatre
 
-my_regex = r"href=[\"\'].*" + mytheatre + r".*[\"\']"
+mytheatres = []
+length = len(sys.argv)
+if length >= 3:
+    mytheatres.append(sys.argv[2])
+if length >= 4:
+    mytheatres.append(sys.argv[3])
+if length >= 5:
+    mytheatres.append(sys.argv[4])
 
-if re.search(my_regex, urllib.urlopen(myurl).read(), re.I):
-    fromaddr = "from@live.com"
-    toaddrs = ["to@gmail.com"];
-    msg = ("From: %s\r\nTo: %s\r\n" % (fromaddr, ", ".join(toaddrs)))
-    msg = msg + "Subject:Tickets available !! for " + mytheatre + "\r\n\r\n"
-    msg = msg + "Tickets available.\n"
-    server = smtplib.SMTP('smtp.live.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.login("from@live.com", "****")
-    server.sendmail(fromaddr, toaddrs, msg)
-    server.quit()
-    print "Tickets available !! for " + mytheatre
-else:
-    print "No Tickets yet :-( for " + mytheatre
+print "Theatre names:" + ", ".join(mytheatres)
+
+for mytheatre in mytheatres:
+
+    my_regex = r"href=[\"\'].*" + mytheatre + r".*[\"\']"
+
+    if re.search(my_regex, urllib.urlopen(myurl).read(), re.I):
+        fromaddr = "from@live.com"
+        toaddrs = ["to@gmail.com"];
+        msg = ("From: %s\r\nTo: %s\r\n" % (fromaddr, ", ".join(toaddrs)))
+        msg = msg + "Subject:Tickets available !! for " + mytheatre + "\r\n\r\n"
+        msg = msg + "Tickets available.\n"
+        server = smtplib.SMTP('smtp.live.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.login("from@live.com", "****")
+        server.sendmail(fromaddr, toaddrs, msg)
+        server.quit()
+        print "Tickets available !! for " + mytheatre
+    else:
+        print "No Tickets yet :-( for " + mytheatre
